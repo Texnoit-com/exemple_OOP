a77be29e84ecd0ef6d36bb69a29ff842fa5b5c40
Maxim Bekurin <BekurinMD@yandex.ru>
2022-05-19 14:14:39 +0500

    ReadME

--- microbit-exemple/main.py
+++ microbit-exemple/main.py
@@ -2,4 +2,11 @@ from microbit import *
 
 # from .training import Running, SportsWalking, Swimming
 
-display.show("12345")
+x = 0
+while True:
+    
+    if accelerometer.was_gesture('shake'):
+        display.show(x)
+        x += 1
+    sleep(1000)
+        
